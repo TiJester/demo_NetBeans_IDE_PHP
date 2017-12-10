43604d34848cb98f3f578722b1bf7e00a88fced3
Grib <Grib@192.168.0.25>
2017-12-10 21:20:39 +0200

    Add txt

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Заголовок</title>
+    </head>
+    <body>
+        <form name="wishList" action="wishlist.php">
+            Show wish list of: <input type="text" name="user" value="" />
+            <input type="submit" value="GO" />
+        </form>
+    </body>
+</html>
 
 
