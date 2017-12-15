8c710c632da27f4ec859d9065346f46885b53198
admin <admin@reserves.ef.local>
2017-12-15 10:12:42 +0200

    Описал класс на подключения, выборку, создание записей в БД

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,74 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    class WishDB extends mysqli 
+    {
+        // единый экземпляр self, общий для всех экземпляров
+        private static $instance = null;
 
+        // конфигурация соедирения с базой данных
+        private $user = "root";//пользователь к базе данных
+        private $pass = ""; //пароль к базе данных
+        private $dbName = "wishlist";
+        private $dbHost = "localhost";
+        
+    // Этот метод должен быть статическим и должен возвращать экземпляр объекта, если объект еще не существует.
+    public static function getInstance() 
+        { 
+        if (!self::$instance instanceof self) { self::$instance = new self; } 
+        return self::$instance; 
+        } 
+    // Методы клонирования и пробуждения предотвращают внешнюю копию экземпляров класса Singleton, 
+    // исключая, таким образом, возможность дублирования объектов. 
+    public function __clone() 
+        {
+        trigger_error('Клонировать нельзя.', E_USER_ERROR); 
+        } 
+        public function __wakeup() 
+        {
+        trigger_error('Дезериализация не допускается.', E_USER_ERROR); 
+        }
+    
+    // private конструктор
+    private function __construct() 
+        {
+        parent::__construct($this->dbHost, $this->user, $this->pass, $this->dbName);
+        if (mysqli_connect_error()) 
+            {
+            exit('Ошибка подключения (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+            }
+        parent::set_charset('utf-8');
+        }
+    
+    //Функции в классе WishDB    
+    //get_wisher_id_by_name для получения идентификатора владельца на основе имени продавца
+    //get_wishes_by_wisher_id для получения списка пожеланий желающего с определенным идентификатором
+    //create_wisher для добавления новой записи wisher к пользователям таблицы
+
+    //Функция требует имя воиска в качестве входного параметра и возвращает идентификатор wisher.
+    public function get_wisher_id_by_name($name) 
+       {
+        $name = $this->real_escape_string($name);
+        $wisher = $this->query("SELECT id FROM wishers WHERE name = '"
+        . $name . "'"); if ($wisher->num_rows > 0)
+            {
+            $row = $wisher->fetch_row();
+            return $row[0];
+            } else
+        return null; 
+        }
+        
+    //Функция требует идентификатора идентификатора в качестве входного параметра и возвращает пожелания, зарегистрированные для пользователя.
+    public function get_wishes_by_wisher_id($wisherID) 
+        {
+        return $this->query("SELECT id, description, due_date FROM wishes WHERE wisher_id=" . $wisherID);
+        }  
+        
+    //Функция создает новую запись в таблице wishers. Функция требует имя и пароль нового wisher в качестве входных параметров и не возвращает никаких данных.
+    public function create_wisher ($name, $password)
+        {
+        $name = $this->real_escape_string($name);
+        $password = $this->real_escape_string($password);
+        $this->query("INSERT INTO wishers (name, password) VALUES ('" . $name . "', '" . $password . "')");
+        }    
+        
+    }
